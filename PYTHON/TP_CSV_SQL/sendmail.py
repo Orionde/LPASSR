2961f254b1ac1b94fe88fed2da85adecfe9fb56a
congiohj <Jorane.Congio-Hollard@etu.upmf-grenoble.fr>
2017-03-05 22:40:33 +0100

    Update sendmail.py

--- PYTHON/TP_CSV_SQL/sendmail.py
+++ PYTHON/TP_CSV_SQL/sendmail.py
@@ -7,7 +7,6 @@ def sendmail(email, login, passwd):
     """
     Arguments : Login, password and email
     Send mail to user
-
     """
 
     fromaddr = "binomejoetfab@gmail.com"
@@ -16,7 +15,9 @@ def sendmail(email, login, passwd):
     msg['From'] = fromaddr
     msg['To'] = toaddr
     msg['Subject'] = "Vos identifiants"
-    body = "Bonjour, \nVous être à présent enregistré dans la base de l'entreprise et pouvez accéder aux différents services.\nLogin : " + login + "\nMot de passe : " + passwd + "\nCordialement"
+    body = "Bonjour, \nVous être à présent enregistré dans la base de \
+        l'entreprise et pouvez accéder aux différents services.\nLogin \
+        : " + login + "\nMot de passe : " + passwd + "\nCordialement"
     msg.attach(MIMEText(body, 'plain'))
 
     # Connexion Serveur SMTP
@@ -26,4 +27,4 @@ def sendmail(email, login, passwd):
     server.login(fromaddr, "Azertyuiop")
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
-    server.quit()
+server.quit()
